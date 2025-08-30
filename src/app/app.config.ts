a685ad7137a0ccf704d2861c9d8706ce7fbf5916
Gadir Mirzayev <mirzayev.gadir@gmail.com>
2025-08-31 02:49:14 +0400

    angular.json ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,7 +7,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(), 
     provideRouter(routes), 
     provideClientHydration(withEventReplay()),
     provideAnimationsAsync()
